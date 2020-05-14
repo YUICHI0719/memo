908536f71b43af07a656275fc004ceccff467034
Yuichi Kanno <yuichi.kanno@nijibox.co.jp>
2020-05-14 18:06:14 +0900

    ページネーション①②③

--- index.php
+++ index.php
@@ -20,7 +20,17 @@
 
 <?php 
   require('dbconnect.php');
-  $memos = $db->query('SELECT * FROM memos ORDER BY id DESC');
+  
+  if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
+    $page = $_REQUEST['page'];
+  } else {
+    $page = 1;
+  }
+  $start = 5 * ($page - 1);
+
+  $memos = $db->prepare('SELECT * FROM memos ORDER BY id DESC LIMIT ?, 5');
+  $memos->bindParam(1, $start, PDO::PARAM_INT);
+  $memos->execute();
 ?>
 
 <article>
@@ -33,6 +43,23 @@
     <time><?php print($memo['created_at']); ?></time>
     <hr>
   <?php endwhile; ?>
+
+  <?php if ($page >= 2): ?> 
+    <a href="index.php?page=<?php print($page - 1); ?>">
+      <?php print($page - 1); ?>ページ目へ
+    </a>
+  <?php endif; ?> 
+  |
+  <?php
+    $counts = $db->query('SELECT COUNT(*) as cnt FROM memos');
+    $count = $counts->fetch();
+    $max_page = ceil($count['cnt'] / 5);
+    if ($page < $max_page):
+  ?> 
+    <a href="index.php?page=<?php print($page + 1); ?>">
+      <?php print($page + 1); ?>ページ目へ
+    </a>
+  <?php endif; ?> 
 </article>
 </main>
 </body>    
